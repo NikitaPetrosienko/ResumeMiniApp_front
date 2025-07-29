c218fb1b1158b4db30f8f492113baf3bc33f93c2
DevOG <petrosienko.nikita@mail.ru>
2025-07-29 12:49:02 +0300

    try fixed (update main.jsx)

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+const tg = window.Telegram.WebApp;
+tg.ready();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
